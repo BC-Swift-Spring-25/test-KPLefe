930e4f5dff31d4f2e42a6d9ff3c623393804a3eb
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 11:49:53 -0500

    Initial Commit

---
+++ SwiftUI_CH_1_2_Print_Buttons_State/SwiftUI_CH_1_2_Print_Buttons_StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CH_1_2_Print_Buttons_StateApp.swift
+//  SwiftUI_CH_1_2_Print_Buttons_State
+//
+//  Created by Kevin Lefebvre on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CH_1_2_Print_Buttons_StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
