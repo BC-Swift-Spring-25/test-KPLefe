0e8b9f82276b40bacb708fe8d982c13d592efd93
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 12:05:33 -0500

    CH1_3 video exercise. Button properties. Spacer sharing. Use of frame and right chevron. use font title2 to get text more full within button

--- SwiftUI_CH1_3_ButtonStyle_Spacer_Frame/ContentView.swift
+++ SwiftUI_CH1_3_ButtonStyle_Spacer_Frame/ContentView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I Am A Programmer!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer() // top of phone down
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                // .framewihe (auto detect args wanted)
+                //.frame(width: <#T##CGFloat?#>, height: <#T##CGFloat?#>)
+                .frame(width:200, height: 200)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            Spacer() // share space equally with above spacer
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Button("Great") {
+                    message = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            //.font(.system(size: 32))
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
