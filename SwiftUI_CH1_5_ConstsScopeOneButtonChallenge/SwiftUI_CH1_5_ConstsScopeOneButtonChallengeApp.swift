3761c18f90938d6b92e37f82fbf5c9a75b7520ce
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 12:37:11 -0500

    Initial Commit

---
+++ SwiftUI_CH1_5_ConstsScopeOneButtonChallenge/SwiftUI_CH1_5_ConstsScopeOneButtonChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CH1_5_ConstsScopeOneButtonChallengeApp.swift
+//  SwiftUI_CH1_5_ConstsScopeOneButtonChallenge
+//
+//  Created by Kevin Lefebvre on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CH1_5_ConstsScopeOneButtonChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
