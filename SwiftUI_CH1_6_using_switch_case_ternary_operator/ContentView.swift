8f74ae6c7e1056594b823d673ded4a0bdaa09dd6
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 13:15:26 -0500

    Use of ternary operator where if/else was used.

--- SwiftUI_CH1_6_using_switch_case_ternary_operator/ContentView.swift
+++ SwiftUI_CH1_6_using_switch_case_ternary_operator/ContentView.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var systemImageName = ""
+    @State private var imageTextName = ""
     var body: some View {
+        Spacer()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: systemImageName)
+                .resizable()
+                .scaledToFit( )
+                .foregroundColor(.orange)
+            Text(imageTextName)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
         }
+        Spacer()
+        HStack {
+            Button("Press Me") {
+                let message1 = "You are awesome"
+                let message2 = "You are Great!"
+                let systemImageName1 = "hand.thumbsup"
+                let systemImageName2 = "sun.max.fill"
+//                if (imageTextName == message1) {
+//                    imageTextName = message2
+//                    systemImageName = systemImageName2
+//                } else {
+//                    imageTextName = message1
+//                    systemImageName = systemImageName1
+//                }
+                // Do above but with ternary operator
+                imageTextName = (imageTextName == message1) ? message2 : message1
+                systemImageName = (systemImageName == systemImageName1) ? systemImageName2 : systemImageName1
+                
+                
+            }
+        }
+        
+        .buttonStyle(.borderedProminent)
+        .tint(.orange)
         .padding()
     }
 }
