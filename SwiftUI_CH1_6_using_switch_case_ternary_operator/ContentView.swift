7884328e841cb08578343bf20e76a6b64536a015
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 13:13:22 -0500

    Initial Commit

---
+++ SwiftUI_CH1_6_using_switch_case_ternary_operator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_CH1_6_using_switch_case_ternary_operator
+//
+//  Created by Kevin Lefebvre on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
