89dbef66e599d70c5514b8aa7323aea020781e7b
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 12:21:21 -0500

    Initial Commit

---
+++ SwiftUI_CH_1_4_DebugConsole_Print_Conditionals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_CH_1_4_DebugConsole_Print_Conditionals
+//
+//  Created by Kevin Lefebvre on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_CH_1_4_DebugConsole_Print_Conditionals/SwiftUI_CH_1_4_DebugConsole_Print_ConditionalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CH_1_4_DebugConsole_Print_ConditionalsApp.swift
+//  SwiftUI_CH_1_4_DebugConsole_Print_Conditionals
+//
+//  Created by Kevin Lefebvre on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CH_1_4_DebugConsole_Print_ConditionalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
