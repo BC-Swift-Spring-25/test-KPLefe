8229fe5fa0dd6498784a2ce303be431acbb84557
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 12:10:39 -0500

    Initial Commit

---
+++ SwiftUI_Challenge_Elvis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Challenge_Elvis
+//
+//  Created by Kevin Lefebvre on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
