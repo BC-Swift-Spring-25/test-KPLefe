7d9648bc8b31ccc7e0b9bc921b0ac0e09d8ecbf7
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 12:45:29 -0500

    Challenge with screenshots. This is Ch1.4 lesson in practice

--- SwiftUI_CH1_5_ConstsScopeOneButtonChallenge/ContentView.swift
+++ SwiftUI_CH1_5_ConstsScopeOneButtonChallenge/ContentView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var systemImageName = ""
+    @State private var imageTextName = ""
     var body: some View {
+        Spacer()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: systemImageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(imageTextName)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
         }
+        Spacer()
+        HStack { //
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great"
+                
+                if (imageTextName == message1) {
+                    imageTextName = message2
+                    systemImageName = "hand.thumbsup"
+                } else {
+                    imageTextName = message1
+                    systemImageName = "sun.max.fill"
+                }
+                
+            }
+        }
+        .buttonStyle(.borderedProminent)
+        .tint(.orange)
+        .font(.title2)
         .padding()
     }
 }
