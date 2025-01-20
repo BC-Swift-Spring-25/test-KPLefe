0e080eba5fa42852379bf53809437bcef33dacd2
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-20 12:27:26 -0500

    Toggle Button. Use local variables in scope where needed. Will show up in code complete in scopes where not needed otherwise

--- SwiftUI_CH_1_4_DebugConsole_Print_Conditionals/ContentView.swift
+++ SwiftUI_CH_1_4_DebugConsole_Print_Conditionals/ContentView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var systemImageName = ""
+    @State private var imageTextName = ""
     var body: some View {
+        Spacer()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: systemImageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(imageTextName)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
         }
+        Spacer()
+        HStack { // Not needed if only one button in row
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great"
+                let systemImageName1 = "hand.thumbsup"
+                let systemImageName2 = "sun.max.fill"
+                
+                
+                if (imageTextName == message1) {
+                    imageTextName = message2
+                    systemImageName = systemImageName2
+                } else {
+                    imageTextName = message1
+                    systemImageName = systemImageName1
+                }
+                
+            }
+        }
+        .buttonStyle(.borderedProminent)
+        .tint(.orange)
+        .font(.title2)
         .padding()
     }
 }
