21bc2f77ad030756bc29f7fb5d4f6be74d7075d4
Kevin Lefebvre <kevin@gbamerica.net>
2025-02-14 09:33:56 -0500

    Building BIP the APP guy. Playsound on tapGesture

--- SwiftUI_Week6_CH3_1_BuildingBipTheGuy/ContentView.swift
+++ SwiftUI_Week6_CH3_1_BuildingBipTheGuy/ContentView.swift
@@ -6,17 +6,50 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image("clown")
+                .resizable()
+                .scaledToFit( )
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                }
+            Spacer()
+            
+            Button {
+                // TODO: Button Action Here
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+             
         }
         .padding()
     }
+    
+    func playSound(soundName: String) {
+            // if sound is playing stop first
+            if (audioPlayer != nil && audioPlayer.isPlaying) {
+                    audioPlayer.stop()
+            }
+            
+            guard let soundFile = NSDataAsset(name: soundName) else {
+                print("😡 Could not read file name \(soundName)")
+                return
+            }
+            
+            do {
+                audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                audioPlayer.play()
+            } catch {
+                print("😡 Error \(error.localizedDescription) creating audioPlayer")
+            }
+        }
+        
 }
 
 #Preview {
