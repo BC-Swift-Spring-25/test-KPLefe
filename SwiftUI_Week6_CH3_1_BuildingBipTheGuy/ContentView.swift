a67f779e6910baefd4c86524d4c387eef11bd252
Kevin Lefebvre <kevin@gbamerica.net>
2025-02-14 09:10:39 -0500

    Initial Commit

---
+++ SwiftUI_Week6_CH3_1_BuildingBipTheGuy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Week6_CH3_1_BuildingBipTheGuy
+//
+//  Created by Kevin Lefebvre on 2/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Week6_CH3_1_BuildingBipTheGuy/SwiftUI_Week6_CH3_1_BuildingBipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Week6_CH3_1_BuildingBipTheGuyApp.swift
+//  SwiftUI_Week6_CH3_1_BuildingBipTheGuy
+//
+//  Created by Kevin Lefebvre on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Week6_CH3_1_BuildingBipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
